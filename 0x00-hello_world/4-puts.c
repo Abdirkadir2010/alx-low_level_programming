0a09a7a60626fa98e4eaf09a924dab979efc0ddd
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-09-08 15:16:31 -0700

    hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.>
+/**
+ *
+ *  *Beginning of  main function
+ *
+ *   *
+ *
+ *    * Return: Nothing (o)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ *  *Beginning of main function - Entry point
+ *
+ *   *
+ *
+ *    * Return nothing
+ *
+ *     */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,]n");
+	return (0);
+}
