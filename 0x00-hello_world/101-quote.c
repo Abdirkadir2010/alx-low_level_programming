e944212c3ddb53c764dcf6a4ab335bba390806f7
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-09-10 06:49:19 -0700

    hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Beginning of main function
+ *
+ * Return: 1 to the system
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful"\n" - Dora Korpar, 2015-10-19\iin", 59);
+	return (1);
+}
