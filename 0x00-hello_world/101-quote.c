e6ba2106bd7142318de00b108a722a19886b2982
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-09-12 10:48:10 -0700

    hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-/**
- * main - Beginning of main function
- *
- * Return: 1 to the system
- */
+/*
+* main - fucntion start here
+*
+* Return: 0 to the system
+*/
 int main(void)
 {
-	write("and that piece of art is useful"\n" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
