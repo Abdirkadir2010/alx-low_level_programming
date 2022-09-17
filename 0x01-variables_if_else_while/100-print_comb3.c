5f1b9ca936291106ca1c6877799d2b2b433af6aa
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-09-17 11:48:59 -0700

    100- 2digit of numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Beginning of main function
+ *
+ * Return: 0 to the system
+ */
+int main(void)
+{
+	int num1, num2;
+
+	num1 = '0';
+
+	while (num1 <= '8')
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			if (!(num1 > num2))
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 != '8' || num2 != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
+
