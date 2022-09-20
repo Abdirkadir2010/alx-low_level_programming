4946d992024834f2bf2da7f66767259b46b4ebae
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-09-20 13:12:49 -0700

    print charcter fo strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,22 @@
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
+ * rev_string -  function taht reverses the string
+ * @s: pointer to string
  *
- *
+ * Return: nothing(void)
  */
 
 void rev_string(char *s)
 {
-	int n;
-
-	n = strlen(s);
-
 	int j;
 
-	for (j = 0; j < n / 2; j++)
+	for (j = len - 1; j >= 0; j--)
 	{
-
-
+		putchar(s[j]);
+	}
+	putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * puts2 - prints charcter in the string
+ * @str: pointer to string
+ *
+ * Return: Nothing(void)
+ */
+void puts2(char *str)
+{
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		if (j % 2 == 0)
+		{
+			putchar(str[j]);
+		}
+	}
+		putchar('\n');
+}
