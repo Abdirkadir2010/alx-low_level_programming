18189305ee2ca7c24cd11a7361d190f58d7b4497
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-09-22 03:25:56 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Beginning of main function
+ *
+ * Return: 0 to system
+ */
+int main(void)
+{
+	times_table();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table - print time table
+ *
+ * Return: Nothing (void)
+ */
+void times_table(void)
+{
+	int j, k, result;
+
+	for (j = 0; j <= 9; j++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+
+		for (k = 0; k <= 9; k++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			result = (j * k);
+
+			if (result <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+				_putchar((result / 10) + '0');
+			_putchar((result % 10) + '0');
+
+
+			if ((result / 10) > 0)
+			{
+				_putchar((result / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
