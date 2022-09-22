44cfce7735f2018ede14abbd2a3a82c75d50c16a
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-09-22 05:47:39 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add the numbers
+ * @num1: input the first number
+ * @num2: input the second number
+ *
+ * Return: the result
+ */
+int add(int num1, int num2)
+{
+	return(num1 + num2);
+}
--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,2 +1,15 @@
 #include "main.h"
-#include
+#include <stdio.h>
+/**
+ * main - Beginning of main function
+ *
+ * Return: 0 to the system
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89,9);
+	printf("%d\n", n);
+	return (0);
+}
