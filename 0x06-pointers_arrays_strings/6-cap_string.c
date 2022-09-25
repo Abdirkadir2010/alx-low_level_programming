3737b87fa899179d192868a1386009f24500baf6
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-09-25 14:01:28 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,39 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * *cap_string - capitalize string
- *
+ * @str: string need to be capitalized
+ * 
  * Return: Return the string
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
 	int j = 0;
 
-	int len = strlen(str);
-	while (str[j] != 0; j < len)
+	while (str[j] != '\0')
 	{
-		if (str[j] >= 'a' && str[j] <= 'z')
+		if (str[j] >= 97 && str[j] <= 122)
 		{
-			str[j] -= 32;
+			if (j == 0)
+			{
+				str[j] -= 32;
+			}
+			if (str[j - 1] == ' ' ||
+		 	    str[j - 1] == '\t' || 
+		   	    str[j - 1] == '\n' || 
+		   	    str[j - 1] == ',' || 
+		   	    str[j - 1] == ';' || 
+		    	    str[j - 1] == '.' || 
+		    	    str[j - 1] == '!' || 
+		    	    str[j - 1] == '?' || 
+		    	    str[j - 1] == '"' || 
+		    	    str[j - 1] == '(' || 
+		      	    str[j - 1] == ')' || 
+		    	    str[j - 1] == '{' || 
+		    	    str[j - 1] == '}')
+				str[j] -= 32;
 		}
-		else
-			str[j] = str [j];
 		j++;
 	}
 	return (str);
