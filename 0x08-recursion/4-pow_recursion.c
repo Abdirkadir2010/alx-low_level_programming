3d8f213023a0e4d8be63d40c1b248dffa67b32c2
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-09-29 12:38:11 -0700

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,17 +4,16 @@
  * calc_pow - calculate function sqrt
  * @i: interger
  * @n: interger
- * Return: evaluate sqrt
+ * Return: return number
  */
-int _calc_pow(int i, int n)
+int calc_pow(int i, int n)
 {
-	if (n == 0 || n == 1)
-		return (n);
-	else if (i * i < n)
-		return (calc_pow(i + 1, n));
-	else if (i * i == n)
+	if (n == 1)
 		return (i);
-	return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (i * calc_pow(i, n - 1));
 }
 /**
  * _pow_recursion - evaluate sqrt
@@ -22,7 +21,7 @@ int _calc_pow(int i, int n)
  * @y: integer num
  * Return: Sgrt_recursion
  */
-int _pow_recursion(int x, int y);
+int _pow_recursion(int x, int y)
 {
 
 	if (y < 0)
