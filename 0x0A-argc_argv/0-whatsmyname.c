dae3cc1e072a58ccf449bd20025d01d92b3cf7bf
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-09-30 12:14:24 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: contain number of commands
+ * @arcv[]: list number of commands
+ * Return: 0
+ */
+int main(int argc, char* argv[])
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
