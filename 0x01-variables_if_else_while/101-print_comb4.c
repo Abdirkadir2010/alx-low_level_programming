35b7aebd206b82718ce4dc1318c032c776ff1fd8
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-10-07 12:03:36 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,25 +10,22 @@ int main(void)
 {
 	int num1, num2, num3;
 
-	num1 = '0';
+	num1 = 0;
 
-	while (num1 <= '7')
+	while (num1 < 8)
 	{
-		for (num2 = '0'; num2 <= 8; num2++)
+		for (num2 = num1 + 1; num2 < 9; num2++)
 		{
-			for (num3 = '0'; num3 <= '9'; num3++)
+			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
-				if (!(num1 >= num2) && !(num1 >= num3) && !(num2 >= num3))
-				{
-					putchar(num1);
-					putchar(num2);
-					putchar(num3);
-					if (num1 != '7' && num2 != '8' && num3 != '9')
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
+
+				if (num1 == 7 && num2 == 8 && num3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
 		}
 		num1++;
@@ -36,4 +33,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
