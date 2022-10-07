527b3e5d43f26b02747279c938e04df5d3ff644e
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-10-07 09:40:12 -0700

    1task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
 #include<unistd.h>
-#include <stdlib.h>
 
 /**
 * main - fucntion start here
 *
-* Return: 0 to the system
+* Return: 1 to the system
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2,
+		"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",
+		59);
+
 	return (1);
 }
