aca7be8db5fcbbc41759cbbdbcbb993e5be0abb3
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-10-15 13:12:12 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print _most_numbers - Function that print numbers
+ *
+ * Return: Nothing
+ */
+void print_most_numbers(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		if (j != 2 && j != 4)
+			_putchar(j + '0');
+		j++;
+	}
+	_putchar('\n');
+}
