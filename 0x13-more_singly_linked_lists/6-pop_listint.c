ce2a326c262cb00d657b0e9e8ff754af95d9594e
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-10-26 10:12:59 -0700

    Task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,17 +9,17 @@
 int pop_listint(listint_t **head)
 {
 	int hd_node;
-	listint_t *h;
 	listint_t c_node;
 
 	if (*head == NULL)
 		return (0);
-	c_node = *head;
-	hd_node = c_node->hd_node;
-	h = c_node->next;
+	
+	hd_node = (*head)->hd_node;
+	c_node = (*head)->next;
 
-	free(c_node);
-	*head = h;
+	free(*head);
+
+	*head = c_node;
 	
 	return (hd_node);
 }
