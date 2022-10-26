af4d070e27177973346b19c3415a79c3ca26931d
Abdirkadir2010 <abdirkadirhassen2010@gmail.com>
2022-10-26 08:34:04 -0700

    Task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ * @head: head of list
+ *
+ * Return: Nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t tmpdt;
+
+	while ((tmpdt = head) != NULL)
+	{
+		head = head->next;
+		free(tmpdt);
+	}
+}
